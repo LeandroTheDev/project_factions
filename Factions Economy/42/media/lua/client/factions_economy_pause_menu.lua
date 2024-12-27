fd679c3a6bfa2e43f38c468863905bd125f5ce98
LeandroTheDev <leandro.schmidt.profissional@gmail.com>
2024-12-26 21:16:25 -0300

    fixed the factions economy shop and trade UI

--- Factions Economy/42/media/lua/client/factions_economy_pause_menu.lua
+++ Factions Economy/42/media/lua/client/factions_economy_pause_menu.lua
@@ -7,9 +7,12 @@ local defaultMainScreen = MainScreen.instantiate
 -- Overwrite the pause menu
 function MainScreen:instantiate()
     -- Default behavior
-    defaultMainScreen(self)
+    defaultMainScreen(self);
 
     if self.inGame then
+        local labelHgt = getTextManager():getFontHeight(UIFont.Large) + 8 * 2;
+        local labelY = self.bottomPanel:getHeight();
+        local labelX = 0;
         -- Add the text for open Shop
         if self.quitToDesktop then
             -- Default options
@@ -17,7 +20,6 @@ function MainScreen:instantiate()
             local core = getCore();
             local width = 800 * FONT_SCALE;
             local height = 600 * FONT_SCALE;
-            local labelHgt = getTextManager():getFontHeight(UIFont.Large) + 8 * 2
             -- Initializing the UI
             self.serverShop = ServerShopUI:new((core:getScreenWidth() - width) / 2, (core:getScreenHeight() - height) / 2,
                 width, height);
@@ -27,16 +29,17 @@ function MainScreen:instantiate()
             self.serverShop:setAnchorBottom(true);
             self:addChild(self.serverShop);
 
-            -- Creating the Labels
-            self.shopOptions = ISLabel:new(self.quitToDesktop.x, self.quitToDesktop.y + labelHgt + 16, labelHgt,
-                getText("IGUI_Shop"), 1, 1, 1, 1, UIFont.Large, true);
-            self.shopOptions.internal = "SHOP";
-            self.shopOptions:initialise();
-            self.bottomPanel:addChild(self.shopOptions);
-            self.shopOptions.onMouseDown = function(item, x, y)
+            labelY = labelY + 16;
+            self.shopLabel = ISLabel:new(labelX, labelY, labelHgt, getText("IGUI_Shop"), 1, 1, 1, 1, UIFont.Large,
+                true);
+            self.shopLabel.internal = "SHOP";
+            self.shopLabel:initialise();
+            self.bottomPanel:addChild(self.shopLabel);
+            self.shopLabel.onMouseDown = function(item, x, y)
                 getSoundManager():playUISound("UIActivateMainMenuItem");
                 MainScreen.instance.serverShop:setVisible(true);
             end;
+            labelY = labelY + labelHgt;
         end
 
         -- Add the text for open the Trading
@@ -53,16 +56,20 @@ function MainScreen:instantiate()
             self.serverTrade:setAnchorBottom(true);
             self:addChild(self.serverTrade);
 
-            local labelHgt = getTextManager():getFontHeight(UIFont.Large) + 8 * 2
-            self.tradeOptions = ISLabel:new(self.quitToDesktop.x, self.shopOptions.y + labelHgt + 16, labelHgt,
-                getText("IGUI_Trade"), 1, 1, 1, 1, UIFont.Large, true)
-            self.tradeOptions.internal = "TRADES"
-            self.tradeOptions:initialise()
-            self.bottomPanel:addChild(self.tradeOptions);
-            self.tradeOptions.onMouseDown = function(item, x, y)
+            -- Creating the Labels
+            labelY = labelY + 16;
+            self.tradeLabel = ISLabel:new(labelX, labelY, labelHgt, getText("IGUI_Trade"), 1, 1, 1, 1, UIFont.Large,
+                true);
+            self.tradeLabel.internal = "TRADE";
+            self.tradeLabel:initialise();
+            self.bottomPanel:addChild(self.tradeLabel);
+            self.tradeLabel.onMouseDown = function(item, x, y)
                 getSoundManager():playUISound("UIActivateMainMenuItem");
                 MainScreen.instance.serverTrade:setVisible(true);
             end
+            labelY = labelY + labelHgt;
         end
+
+        self.bottomPanel:setHeight(labelY);
     end
 end
